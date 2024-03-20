cdb5e9a4b9c5e4dc1a9e4159e54e007266429fd7
Paolo <paolo.gava@gmail.com>
2024-03-20 22:08:50 +1100

    Add database management

---
+++ BotLife.Db/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using DbUp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json");
+        var configuration = builder.Build();
+        
+        var connectionString = configuration.GetConnectionString("BotLifeDb");
+        
+        EnsureDatabase.For.SqlDatabase(connectionString);
+
+        var upgrader =
+            DeployChanges.To
+                .SqlDatabase(connectionString)
+                .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                .LogToConsole()
+                .WithTransaction()
+                .Build();
+
+        var result = upgrader.PerformUpgrade();
+
+        if (!result.Successful)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(result.Error);
+            Console.ResetColor();
+#if DEBUG
+            Console.ReadLine();
+#endif                
+            return;
+        }
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Success!");
+        Console.ResetColor();
+    }
+}
